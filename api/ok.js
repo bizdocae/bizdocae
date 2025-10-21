725b4f9a1092a146f6e07b68ea1af25b02e54db5
mayedalfalasi <majed.oalfalasi@gmail.com>
2025-10-21 07:39:41 +0000

    cleanup duplicate API routes (keep .cjs only)

--- api/ok.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.status(200).end(JSON.stringify({ ok: true, t: Date.now() }));
-};
-module.exports.config = { runtime: "nodejs" };

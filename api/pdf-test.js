9cde1af4318c02d60133858b260198eecc915fd0
mayedalfalasi <majed.oalfalasi@gmail.com>
2025-10-22 07:32:09 +0000

    add /api/pdf-test smoke endpoint

--- api/pdf-test.js
+++ api/pdf-test.js
@@ -4,7 +4,6 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") return res.status(204).end();
 
-  // Minimal valid PDF bytes (%PDF-1.4 one blank page)
   const pdf = Buffer.from("%PDF-1.4\n%âãÏÓ\n1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 595.28 841.89] >>\nendobj\nxref\n0 4\n0000000000 65535 f \n0000000015 00000 n \n0000000060 00000 n \n0000000116 00000 n \ntrailer\n<< /Root 1 0 R /Size 4 >>\nstartxref\n195\n%%EOF\n","binary");
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Disposition", 'attachment; filename="smoke.pdf"');
